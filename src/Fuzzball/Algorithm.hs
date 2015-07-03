cdf305b03f30438893375c3b0901d948f8a48309
fro_ozen <fro_ozen@gmx.de>
2015-07-03 23:20:48 +0200

    Initial commit

---
+++ src/Fuzzball/Algorithm.hs
@@ -0,0 +1,34 @@
+{-|
+Module      : Fuzzball.Algorithm
+Description : Implementation of a fuzzy matching algorithm
+
+The Fuzzball.Algorithm module implements the actual fuzzy matching algorithm.
+-}
+
+module Fuzzball.Algorithm
+    ( fuzzyMatch
+    , MatchRating(rating, indexes)
+    ) where
+
+-- | The rating of the quality of a fuzzy match
+data MatchRating = MatchRating {
+      rating  :: Int
+    , indexes :: [Int]
+    } deriving (Eq, Ord, Show)
+
+-- | Fuzzily match a pattern over a string
+fuzzyMatch :: String -> String -> Maybe MatchRating
+fuzzyMatch [] _ = Just . MatchRating 0 $ []
+fuzzyMatch _ [] = Nothing
+fuzzyMatch (p:ps) (c:cs)
+    | p == c = fmap found . fmap push . fuzzyMatch ps $ cs
+    | otherwise = fmap push . fuzzyMatch (p:ps) $ cs
+    where -- Increment the indices
+          push mr@(MatchRating _ indexes) =
+            let indexes' = map (+1) indexes
+            in  mr { indexes = indexes' }
+
+          -- Update the rating and add a new index
+          found mr@(MatchRating rating indexes) =
+            let rating' = rating + if null indexes then 0 else head indexes
+            in mr { rating = rating', indexes = 0:indexes }
---
+++ src/Main.hs
@@ -0,0 +1,11 @@
+{-|
+Module      : Main
+Description : The Main module of the tool
+
+The Main module of the fuzzy matching tool.
+-}
+
+module Main where
+
+main :: IO ()
+main = putStrLn "Nothing to see here"
